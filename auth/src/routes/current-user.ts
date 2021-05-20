242bcd3e36c00cfea4ca218fe42f983b8a7e490b
Diego Castro <diegh.castro@gmail.com>
2021-05-19 19:30:44 -0500

    seccion de estrategias de autorizacion terminada

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,9 +1,14 @@
 import express from 'express';
+import { currentUser } from "../middlewares/current-user";
 
 const router = express.Router();
 
-router.get('/api/users/currentuser', (req, res  ) =>{
-    res.send("hola");
+router.get('/api/users/currentuser',
+    currentUser,    
+(req, res  ) =>{
+    
+    res.send( { currentUser: req.currentUser || null } )
+
 });
 
 
